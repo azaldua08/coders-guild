f926e294511bdeb6c995f9e14b8c13cc8d0a87c5
azaldua08 <amiel_zaldua@yahoo.com>
2018-08-03 12:41:42 +0800

    Updated CORS config
    Update CORS config to allow origins *

--- src/main/java/com/magenic/gamify/security/WebSecurityConfig.java
+++ src/main/java/com/magenic/gamify/security/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.magenic.gamify.security;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @EnableWebSecurity
 @Configuration
@@ -22,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private CustomUserDetailsService userDetailsService;
-	
+	    
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth)
 	  throws Exception {
@@ -49,11 +53,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
+    	 final CorsConfiguration configuration = new CorsConfiguration();
+         configuration.setAllowedOrigins(Collections.unmodifiableList(Arrays.asList("*")));
+         configuration.setAllowedMethods(Collections.unmodifiableList(Arrays.asList("HEAD",
+                 "GET", "POST", "PUT", "DELETE", "PATCH")));
+         // setAllowCredentials(true) is important, otherwise:
+         // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+         configuration.setAllowCredentials(true);
+         // setAllowedHeaders is important! Without it, OPTIONS preflight request
+         // will fail with 403 Invalid CORS request
+         configuration.setAllowedHeaders(Collections.unmodifiableList(Arrays
+        		 .asList("Authorization", "Cache-Control", "Content-Type")));
         UrlBasedCorsConfigurationSource source = new
                 UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+    
     
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
